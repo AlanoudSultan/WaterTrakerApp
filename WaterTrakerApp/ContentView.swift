518473be3d9f067d95f7508b5ddf670856f1a439
Alanoud <abaalkhailalanoud@gmail.com>
2024-09-26 17:31:03 +0300

    button

--- WaterTrakerApp/ContentView.swift
+++ WaterTrakerApp/ContentView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Text("Water Traker App 💦")
+            
+           
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/
+                }
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                Text("Cups to drink per day 0")
+            }
+            Button("Continue") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
+        
         .padding()
     }
 }
@@ -22,3 +32,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
