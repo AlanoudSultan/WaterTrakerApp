7aae26349701c0e732b1ae9e30fff86a97bef32f
Alanoud <abaalkhailalanoud@gmail.com>
2024-09-26 17:17:00 +0300

    Initial Commit

---
+++ WaterTrakerApp/WaterTrakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrakerAppApp.swift
+//  WaterTrakerApp
+//
+//  Created by Alanoud Abaalkhail on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
